8d018f6e15b74f01feb6f3e7b42ce94d1fc65663
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-24 17:13:01 -0600

    folders

---
+++ src/CreateDemonPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CreateDemonPage() {
+  return (
+    <div>CreateDemonPage</div>
+  );
+}
---
+++ src/DemonListPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DemonListPage() {
+  return (
+    <div>DemonListPage</div>
+  );
+}
