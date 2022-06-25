9aeae53567ce485b3f17b54f4551d32319517e6e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-24 18:15:55 -0600

    fetch

--- src/services/FetchUtils.js
+++ src/services/FetchUtils.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import { client } from './client';
 
-export default function FetchUtils() {
-  return (
-    <div>FetchUtils</div>
-  );
+export async function createDemon(demon) {
+  const { data } = await client.from('demons').insert(demon).single();
+
+  return data;
+}
+
+export async function getDemons() {
+  const { data } = await client.from('demons').select('*');
+
+  return data;
+}
+
+export async function updateDemon(demon, id) {
+  const { data } = await client.from('demons').update(demon).match({ id : id }).single();
+
+  return data;
+}
+
+export async function deleteDemon(id) {
+  const { data } = await client.from('demons').delete.match({ id : id }).single();
+
+  return data;
+}
+
+export async function getDemonById(id) {
+  const { data } = await client.from('demons').select('*').match({ id }).single();
+
+  return data;
+}
+
+export async function signUp(email, password) {
+  const { user } = await client.auth.signUp({
+    email: email,
+    password: password
+  });
+  return user;
+}
+
+export async function signIn(email, password) {
+  const { user } = await client.auth.signIn({
+    email: email,
+    password: password
+  });
+  return user;
 }
