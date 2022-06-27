cc4d307d8c97a1de673221c6600d2689a2899596
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-27 13:02:01 -0600

    css create page

--- src/CreateDemonPage.js
+++ src/CreateDemonPage.js
@@ -27,30 +27,30 @@ export default function CreateDemonPage() {
   }
 
   return (
-    <div>
+    <div className='monster'>
       <header>MONSTER CATALOG</header>
-      <form onSubmit={handleSubmit}>
-        <label className='label'>
+      <form className='monsterForm' onSubmit={handleSubmit}>
+        <label className='labels'>
           Monster Name
           <input onChange={e => setName(e.target.value)} value={name} />
         </label>
-        <label className='label'>
+        <label className='labels'>
           Monster Type
           <input onChange={e => setType(e.target.value)} value={type} />
         </label>
-        <label className='label'>
+        <label className='labels'>
           Last Seen
           <input onChange={e => setArea(e.target.value)} value={area} />
         </label>
-        <label className='label'>
+        <label className='labels'>
           How do you Kill them?
           <input onChange={e => setDeathMethod(e.target.value)} value={deathMethod} />
         </label>
-        <label className='label'>
+        <label className='labels'>
           Are they Dead?
           <input onChange={e => setKilled(e.target.value)} value={killed} />
         </label>
-        <button className='button'>ENTER DATA</button>
+        <button className='button1'>ENTER DATA</button>
       </form>
     </div>
   );
