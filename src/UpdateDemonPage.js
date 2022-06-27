4419346d1ac5be440a868308f2414b74412f1997
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-27 13:40:35 -0600

    update demon function

--- src/UpdateDemonPage.js
+++ src/UpdateDemonPage.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { deleteDemon, getDemonById, updateDemon } from './services/FetchUtils';
+import { useHistory, useParams } from 'react-router-dom';
+
 
 export default function UpdateDemonPage() {
+  const { push } = useHistory();
+  const { id } useParams();
+  const [name, setName] = useState('');
+  const [type, setType] = useState('');
+  const [area, setArea] = useState('');
+  const [deathMethod, setDeathMethod] = useState('');
+  const [killed, setKilled] = useState('');
+
+  useEffect(() => {
+    async function doFetch() {
+      const monster = await getDemonById(id);
+      setName(monster.name);
+      setType(monster.type);
+      setArea(monster.area);
+      setDeathMethod(monster.deathMethod);
+      setKilled(monster.setKilled);
+    }
+    doFetch();
+  }, [id]);
+
+  async function handleDeleteDemon() {
+    await deleteDemon(id);
+    push('/demons');
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    
+    await updateDemon({
+      demonName: name,
+      demonType: type,
+      lastSeen: area,
+      killMethod: deathMethod,
+      killedMonster: killed,
+    }, id)
+
+    setName('');
+      setType('');
+      setArea('');
+      setDeathMethod('');
+      setKilled('');
+
+      push('/demons')
+  }
+
+
   return (
-    <div>UpdateDemonPage</div>
+    <div>
+      <header>UPDATE MONSTER DATE</header>
+      <form onSubmit={handleSubmit}>
+      <label className='labels'>
+          Monster Name
+          <input onChange={e => setName(e.target.value)} value={name} />
+        </label>
+        <label className='labels'>
+          Monster Type
+          <input onChange={e => setType(e.target.value)} value={type} />
+        </label>
+        <label className='labels'>
+          Last Seen
+          <input onChange={e => setArea(e.target.value)} value={area} />
+        </label>
+        <label className='labels'>
+          How do you Kill them?
+          <input onChange={e => setDeathMethod(e.target.value)} value={deathMethod} />
+        </label>
+        <label className='labels'>
+          Are they Dead?
+          <input onChange={e => setKilled(e.target.value)} value={killed} />
+        </label>
+        <button className='button2'>EDIT DATA</button>
+      </form>
+        <button className='button2' onClick={handleDeleteDemon}>DELETE DATA</button>
+    </div>
   );
 }
