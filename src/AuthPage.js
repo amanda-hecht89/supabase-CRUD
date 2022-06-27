98c9e1fbfd5b23ec442711434606cdb42f8adbd8
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-27 10:56:38 -0600

    auth page written and showing on page

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signIn, signUp } from './services/FetchUtils';
 
-export default function AuthPage() {
+export default function AuthPage({ setUser }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+  const [error, setError] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const user = await signUp(email, password);
+      setUser(user);   
+    } catch (e) {
+      setError(e.message);
+    }
+  }
+  
   return (
-    <div>AuthPage</div>
+    <div>
+      <h3>Winchesters Guide to Monsters</h3>
+      <h1 className='error'>{error}</h1>
+      <form className='up' onSubmit={handleSubmit}>
+        <p>SIGN UP</p>
+        <label>email
+          <input onChange={e => setEmail(e.target.value)} value={email} type="email" />
+        </label>
+        <label>password
+          <input onChange={e => setPassword(e.target.value)} value={password} type="password" />
+        </label>
+        <button>SIGN UP</button>
+      </form>
+      <form className='in' onSubmit={handleSubmit}>
+        <p>SIGN IN</p>
+        <label>email
+          <input onChange={e => setSignInEmail(e.target.value)} value={signInEmail} type="email" />
+        </label>
+        <label>password
+          <input onChange={e => setSignInPassword(e.target.value)} value={signInPassword} type="password" />
+        </label>
+        <button>SIGN IN</button>
+      </form>
+    </div>
   );
 }
