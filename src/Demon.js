364ab60d64a61c2ea21bb84f80bbdca0994c8d89
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-27 15:04:14 -0600

    demon function

--- src/Demon.js
+++ src/Demon.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Demon() {
-  return (
-    <div>Demon</div>
-  );
+export default function Demon({ monster }) {
+  return <Link to={`/demons/${monster.id}`}>
+    <p>
+      {monster.name} is a {monster.type}. Found in {monster.area}. Can be killed 
+        by {monster.deathMethod}.
+    </p>
+  </Link>;
 }
--- src/DemonListPage.js
+++ src/DemonListPage.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getDemons } from './services/FetchUtils';
+import { Demon } from './Demon';
+
+
 
 export default function DemonListPage() {
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    async function doFetch() {
+      const monsters = await getDemons();
+      setMonsters(monsters);
+    }
+    doFetch();
+  }, []);
+
   return (
-    <div>DemonListPage</div>
+    <div monsterList>
+      {
+        monsters.map((monster, i) => <Demon monster={monster} key={monster.name + monster.type + monster.area + monster.deathMethod + monster.killed + i} />)
+      }
+    </div>
   );
 }
